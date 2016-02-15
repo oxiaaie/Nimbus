88d40ae332da5904918f0dedbc539a9ebac1146c
Daniel Feldman <m.f.g.witchcraft@gmail.com>
2016-02-15 23:01:30 +0200

    Improving the code

--- nimbus.py
+++ nimbus.py
@@ -7,6 +7,7 @@ submissions = r.get_subreddit('netsec').get_new(limit=5)
 for submission in submissions:
     print "--------------------------------------------"
     print "Title: ", submission.title
-    print "Link: ", submission.permalink
+    print "Link to Reddit: ", submission.permalink
+    print "External Url: ", submission.url
     print
     print submission.selftext
