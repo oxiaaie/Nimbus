dc905416a48f5cbb0395a8553803a68a508cc179
Daniel Feldman <m.f.g.witchcraft@gmail.com>
2016-02-16 16:45:06 +0200

    fixing if statment

--- nimbus.py
+++ nimbus.py
@@ -10,12 +10,13 @@ args = parser.parse_args()
 r = praw.Reddit(user_agent='grabbing posts from favorite subreddits by user /u/Wh04m3y3')
 if (args.limit):
     submissions = r.get_subreddit('netsec+pwned').get_new(limit=(args.limit))
-
-for submission in submissions:
-    print "--------------------------------------------"
-    print "Title: ", submission.title
-    print "Link to Reddit: ", submission.permalink
-    print "External Url: ", submission.url
-    print
-    print submission.selftext
+    for submission in submissions:
+        print "--------------------------------------------"
+        print "Title: ", submission.title
+        print "Link to Reddit: ", submission.permalink
+        print "External Url: ", submission.url
+        print
+        print submission.selftext
+else:
+    print args
 
