dd37f54be71e447b74b4aa4175c5d0a3ec31fb9e
Daniel Feldman <m.f.g.witchcraft@gmail.com>
2016-02-16 14:01:56 +0200

    added argument from user to limit posts by -l

--- nimbus.py
+++ nimbus.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 
 import praw
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--limit','-l', type=int,
+                           help='number to limit posts')
+args = parser.parse_args()
 
 r = praw.Reddit(user_agent='grabbing posts from favorite subreddits by user /u/Wh04m3y3')
-submissions = r.get_subreddit('netsec+pwned').get_new(limit=25)
+submissions = r.get_subreddit('netsec+pwned').get_new(limit=(args.limit))
 for submission in submissions:
     print "--------------------------------------------"
     print "Title: ", submission.title
